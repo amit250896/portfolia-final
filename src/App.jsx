d532ecc174bb6d5a2af6acbff15b971a574ee1d4
amit250896 <103573191+amit250896@users.noreply.github.com>
2022-06-30 20:15:19 +0530

    first stage
    portfolio amit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Portfolio from './components/portfolio/Portfolio'
+import Nav from './components/nav/Nav'
+import Experience from './components/experience/Experience'
+import Service from './components/service/Service'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+    return <>
+
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Service />
+        <Portfolio />
+        <Contact />
+        <Footer />
+
+    </>
+}
+
+export default App
